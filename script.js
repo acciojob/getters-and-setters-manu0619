83ebbc5e4a02af669d3efe21ba9e7adda2668c42
manu0619 <125184498+manu0619@users.noreply.github.com>
2025-01-05 23:39:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
-//complete this code
-class Person {}
+// Base class: Person
+class Person {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-class Student extends Person {}
+  // Getter for name
+  get name() {
+    return this._name;
+  }
 
-class Teacher extends Person {}
+  // Setter for age
+  set age(age) {
+    if (age < 0) {
+      console.log("Age cannot be negative");
+    } else {
+      this._age = age;
+    }
+  }
+}
+
+// Subclass: Student
+class Student extends Person {
+  study() {
+    console.log(`${this.name} is studying`);
+  }
+}
+
+// Subclass: Teacher
+class Teacher extends Person {
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
+}
 
 // Do not change the code below this line
 window.Person = Person;
